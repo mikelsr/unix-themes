032bbf1c63baa359be692378c2c7d640b11441be
Mikel <mikel@midgard.deusto.es>
2017-10-05 19:21:55 +0200

    Improve color visualizer

--- color-visualizer.zsh
+++ color-visualizer.zsh
@@ -3,7 +3,7 @@
 # Visualizes all default 255 colors on terminal, or specific ones if
 # passed as parameters.
 
-function print_color() {
+function print_color {
     COLOR=$1
     for STYLE in "38;5"
     do 
@@ -17,10 +17,21 @@ function print_color() {
 if [ $# -eq 0 ]; then
     COLORS=( $(seq 1 255) ) 
 else
-    COLORS=$@
+    COLORS=($@)
 fi
 
 for COLOR in $COLORS
 do
-    print_color $COLOR
+    # check if COLOR is a valid number
+    case ${COLOR#[-+]} in
+        # errors will keep color of predecesor
+        *[!0-9]|'') echo "*${COLOR} is not an uint";;
+        # COLOR must be < 256
+        * ) if [ $COLOR -lt 256 ]; then
+                print_color $COLOR
+            else
+                echo "*${COLOR} > 255; has no associated color"
+            fi;;
+    esac
 done
+
